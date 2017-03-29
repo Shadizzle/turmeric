4761c04b6e46989c790e0242cd89ac721b92d2dd
Shadizzle <mail@shayden.me>
2017-03-29 14:49:32 -0300

    Basic proof of concept in turmeric.new

--- src/turmeric/new.clj
+++ src/turmeric/new.clj
@@ -1,7 +1,6 @@
 (ns turmeric.new)
 
-(defn- to-sym [word]
-  (println word)
+(defn- ->sym [word]
   (-> word name symbol))
 
 (defn- map->vec [binds]
@@ -10,11 +9,14 @@
 (defrecord Lambda [deps binds body])
 
 (defn bind [{:keys [deps binds body]} new-binds]
-  (let [binds (into binds #((do (println %) (update % 0 to-sym))) new-binds)]
+  (let [binds (into binds (map #(update % 0 ->sym)) new-binds)]
     (->Lambda deps binds body)))
 
-(defn beta-reduction [{:keys [binds body]}]
+(defn beta-reduce [{:keys [binds body]}]
   (eval `(let ~(map->vec binds) ~body)))
 
-(defmacro spice [deps body]
-  (->Lambda deps {} body))
+(defmacro lambda [deps body]
+  `(identity ~(->Lambda deps {} body)))
+
+(defmacro defer [name deps body]
+  `(def ~name ~(->Lambda deps {} bodys)))
