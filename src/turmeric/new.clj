c9b40f89094531b34864e4344bc4c87b1174ac78
Shadizzle <mail@shayden.me>
2017-04-03 18:26:33 -0300

    Refactored add fucntion

--- src/turmeric/new.clj
+++ src/turmeric/new.clj
@@ -9,8 +9,8 @@
 (defrecord Spice [deps binds body])
 
 (defn add [{:keys [binds] :as spice} new-binds]
-  (let [binds (into binds (map #(update % 0 ->sym)) new-binds)]
-    (assoc spice :binds binds)))
+  (let [sym<-key (map #(update % 0 ->sym))]
+    (update spice :binds #(into % sym<-key new-binds))))
 
 (defn mix [{:keys [binds body]}]
   (eval `(let ~(map->vec binds) ~body)))
