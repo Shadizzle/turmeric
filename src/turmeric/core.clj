ddda9daa11cb461edb31385a78cd382aac42f6ff
Shadizzle <mail@shayden.me>
2017-04-09 18:28:24 -0300

    Complete rewrite Still need to revise deps, rewrite docs and write new tests

--- src/turmeric/core.clj
+++ src/turmeric/core.clj
@@ -1,34 +1,37 @@
-(ns turmeric.core
-  (:require [clojure.set :refer [subset? intersection difference]]
-            [turmeric.exceptions :as e]))
+(ns turmeric.new)
 
-(defn- to-sym [v]
-  (-> v name symbol))
+(defn- ->sym [word]
+  (-> word name symbol))
 
-(defn- keys->syms [m]
-  (map to-sym (keys m)))
+(defn- map->vec [binds]
+  (into [] cat binds))
 
-(defn- binds->vector [bindings]
-  (reduce-kv #(conj %1 (to-sym %2) %3) [] bindings))
+(defn- vec->map [binds]
+  (into {} (map vec) (partition 2 binds)))
 
-(defn spice* [deps binds fbody]
-  (let [deps-set  (set deps)
-        binds-set (-> binds keys->syms set)]
-    (let [undeclared (difference binds-set deps-set)]
-      (if (seq undeclared)
-        (throw (e/undeclared-binding undeclared deps binds))))
-    (if (= deps-set binds-set)
-      (eval `(let ~(binds->vector binds) ~fbody))
-      (fn [new-binds]
-        (let [new-binds-set (-> new-binds keys->syms set)
-              bound-twice   (intersection new-binds-set binds-set)]
-          (if (seq bound-twice)
-            (throw (e/bindings-already-passed bound-twice deps binds))))
-        (let [binds (merge new-binds binds)]
-          (spice* deps binds fbody))))))
+(defrecord DeferredExpression [needs bound body])
 
-(defmacro spice [deps fbody]
-  `(identity ~(spice* deps {} fbody)))
+(defmacro spice [needs body]
+  `(identity ~(->DeferredExpression needs {} body)))
 
-(defmacro defer [fname deps fbody]
-  `(def ~fname ~(spice* deps {} fbody)))
+(defmacro defspice [name needs body]
+  `(def ~name ~(->DeferredExpression needs {} body)))
+
+(defn add
+  ([defer key val & binds]
+   (add defer (vec->map (into [key val] binds))))
+  ([{:keys [bound] :as defer} binds]
+   (let [syms<-keys (map #(update % 0 ->sym))]
+     (update defer :bound #(into % syms<-keys binds)))))
+
+(defn mix
+  ([defer binds]
+   (mix (add defer binds)))
+  ([defer key val & binds]
+   (mix (apply add defer key val binds)))
+  ([{:keys [needs bound body] :as defer}]
+   (if (= needs (keys bound))
+     (eval `(let ~(map->vec bound) ~body))
+     (let [unbound-needs (filter (-> bound keys set complement) needs)
+           new-body     `(let ~(map->vec bound) ~body)]
+       (eval `(spice ~(vec unbound-needs) ~new-body))))))
